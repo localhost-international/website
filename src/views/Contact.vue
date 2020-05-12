7820f09a3b036d0e6fe17d6c3c1aaf5e2200f828
Leslie Owusu-Appiah <leslie@blank.works>
2020-05-12 13:48:24 +0100

    Secondary commit

---
+++ src/views/Contact.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="work">
+    <router-link to="/">&lt;-</router-link>
+    <h1>Contact</h1><br />
+    <p>
+      <a href="mailto:leslie@localhost.international">Email</a>
+    </p>
+  </div>
+</template>
+
+<style lang="scss">
+.work {
+  p {
+    cursor: pointer;
+  }
+}
+</style>
---
+++ src/views/Experiments.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="experiments">
+    <router-link to="/">&lt;-</router-link>
+    <h1>Experiments</h1><br />
+    <p>Soon...</p>
+  </div>
+</template>
