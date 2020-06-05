5e1376695ff6ce5f46b83539914c63088315b32a
Leslie Owusu-Appiah <leslie@blank.works>
2020-06-05 22:57:07 +0100

    Septenary commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
     <nav>
       <h1>localhost&nbsp;<span aria-hidden="true">🌐</span></h1>
       <p>is a boutique research <span aria-hidden="true">📚</span> and development <span aria-hidden="true">🛠</span> studio based in London, that builds applications and experiences.</p>
-      <p>Currently crafting lab <router-link to="/experiments">experiments</router-link> and decentralised web <router-link to="/projects">projects</router-link>.</p><br />
+      <p>Currently crafting <router-link to="/experiments">experiments</router-link> and decentralised web <router-link to="/projects">projects</router-link>.</p><br />
       <p>Funded by paid <router-link to="/work">work</router-link>.</p><br />
       <p>Get in <a href="mailto:leslie@localhost.international">contact</a><!--router-link to="/contact">contact</router-link-->.</p>
       <p><span aria-hidden="true">✌️</span></p>
