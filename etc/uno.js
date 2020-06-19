6b2a4af715e84d00d917326efa639165623b775a
Leslie Owusu-Appiah <leslie@blank.works>
2020-06-19 19:01:27 +0100

    Updates

--- etc/uno.js
+++ etc/uno.js
@@ -1,6 +1,4 @@
 window.onload = () => {
-  console.log('Uno reverse');
-  alert('Uno reverse');
-  document.querySelector('html').style.opacity = '.5 !important';
-  document.querySelector('body').style.opacity = '.5 !important';
+  console.log('...');
+  window.location = 'http://leslieoa.com/'
 };
