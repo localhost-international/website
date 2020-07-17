a26a1361cc34ce89d92a4007e8c7521e2015a9e1
Leslie Owusu-Appiah <leslie@blank.works>
2020-07-17 21:18:13 +0100

    Rudimentary pageTransitions

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,4 +1,8 @@
+const body = document.body
 const mainContainer = document.querySelector('main')
+
+const domainURI = `${window.top.location.host.toString()}`
+const internalLinksRegex = `a[href^="${domainURI}"], a[href^="/"]`
 
 
 window.onload = () => {
@@ -8,44 +12,53 @@ window.onload = () => {
     window.location.toString()
   )
 }
- 
 
-const anchorLinks = document.querySelectorAll('a')
 
-anchorLinks.forEach((link) => {
-  link.addEventListener('click', (evt) => {
-    evt.preventDefault()
-    const anchorLinkUrl = link.href
-    console.log('anchorLinkUrl', anchorLinkUrl)
 
-		fetch(anchorLinkUrl)
-			.then((resp) => {
-        resp.text().then((text) => {
-          console.log('fetched', text)
-
-          const source = 'main', target = 'main'
-
-          const targetContainer = document.querySelector(target)
-          const partialTemp = document.createElement('div')
-          
-					partialTemp.innerHTML = text
-					const partial = partialTemp.querySelector(source)
-					
-					console.log('load::fetch::', partial)
-
-          targetContainer.replaceWith(partial)
-          
-          history.pushState(
-            { content: partial.innerHTML }, 
-            '', 
-            anchorLinkUrl
-          )
-
-        })
-      })
-
+function load(anchorLinkUrl) {
+  fetch(anchorLinkUrl)
+  .then((resp) => {
+    resp.text().then((text) => {
+      console.log('fetched', text)
+      render(text, anchorLinkUrl)
+    })
   })
-})
+}
+
+
+function render(text, anchorLinkUrl) {
+  const source = 'main', target = 'main'
+  const targetContainer = document.querySelector(target)
+  const partialTemp = document.createElement('div')
+  partialTemp.innerHTML = text
+  const partial = partialTemp.querySelector(source)
+  console.log('load::fetch::', partial)
+  targetContainer.replaceWith(partial)
+  // prepareLinks(body.querySelectorAll(internalLinksRegex))
+  history.pushState(
+    { content: partial.innerHTML }, 
+    '', 
+    anchorLinkUrl
+  )
+  body.classList.remove('fade-out')
+  body.classList.add('fade-in')
+}
+
+
+const anchorLinks = document.querySelectorAll(internalLinksRegex)
+
+function prepareLinks(anchorLinks) {
+  anchorLinks.forEach((link) => {
+    link.addEventListener('click', (evt) => {
+      evt.preventDefault()
+      body.classList.add('fade-out')
+      const anchorLinkUrl = link.href
+      console.log('anchorLinkUrl', anchorLinkUrl)
+      load(anchorLinkUrl)
+    })
+  })  
+}
+prepareLinks(anchorLinks)
 
   
 
