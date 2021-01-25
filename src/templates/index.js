ce786e605d6681e5d64c39f23559245c35a76220
Leslie Owusu-Appiah <leslie@localhost.international>
2021-01-25 19:32:04 +0000

    Remove compiled JS files

--- src/templates/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.defaultTemplate = exports.indexTemplate = void 0;
-var index_html_1 = require("./index.html");
-Object.defineProperty(exports, "indexTemplate", { enumerable: true, get: function () { return index_html_1.template; } });
-var default_html_1 = require("./default.html");
-Object.defineProperty(exports, "defaultTemplate", { enumerable: true, get: function () { return default_html_1.template; } });
--- src/templates/partials/hello-header.html.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.template = void 0;
-exports.template = function () {
-    return "<!--\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\n\tlocalhost %\n\n\tHello human.\n\tThe frontend code used on this website is presented in a human readable format.\n\tWhy? Well: why not!?\n\n\tIn the late 90s (when I started to gain curiosity into how websites were built),\n\t\"View Source\" was an invaluable tool. These days, with minification and obfuscation \n\tused by bundlers and builders in web frameworks, it's harder than ever for someone starting \n\tout to learn \"how things are made\". Hopefully this will help.\n\n\tThis website can also be viewed without JavaScript (or with a text based web browser).\n\tI'm actively working to improve accessibility for tools such as screen readers (e.g. VoiceOver, et al), \n\tso please let me know if this isn't the case.\n\n\tSpotted a bug or wanted to ask a question?\n\n\t* Visit https://github.com/localhost-international/website/issues\n\t* ...or email: leslie@localhost.international\n\n\tHappy hacking. \n\n\tlocalhost % " + ("" + new Date().getFullYear()) + "\n\n-->";
-};
